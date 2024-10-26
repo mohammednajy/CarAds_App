9c692629a2e17f0f3b54332ca274659a6365da2b
ahed <ahedeid2000@gmail.com>
2024-10-26 10:07:50 +0200

    Add custom card widget

--- lib/core/commonWidgets/main_container.dart
+++ lib/core/commonWidgets/main_container.dart
@@ -55,3 +55,51 @@ class MainContainer extends StatelessWidget {
     );
   }
 }
+
+class MainCard extends StatelessWidget {
+  final String? subtitle;
+  final Color backgroundColor;
+
+  final Color? shadowColor;
+  final VoidCallback? onTap;
+  final Widget child;
+  final double top;
+  final double bottom;
+  final double left;
+  final double right;
+
+  const MainCard({
+    super.key,
+    this.subtitle,
+    this.backgroundColor = ColorManager.white,
+    this.onTap,
+    this.shadowColor,
+    required this.child,
+    this.top = 0,
+    this.bottom = 0,
+    this.left = 0,
+    this.right = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        margin:
+            EdgeInsets.only(bottom: bottom, left: left, right: right, top: top),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppSize.borderRadius)),
+        color: backgroundColor,
+        shadowColor: shadowColor,
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: AppPadding.paddingContainerHorizontal,
+              vertical: AppPadding.paddingContainerVertical),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
