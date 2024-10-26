30cb389decda977f9962a38d988eaf1e11c0e59e
ahed <ahedeid2000@gmail.com>
2024-10-26 10:02:45 +0200

    Amendment to Sizes extension and context passing

--- lib/core/utils/extensions/sized_box.dart
+++ lib/core/utils/extensions/sized_box.dart
@@ -1,16 +1,33 @@
 //  What did you mean in your comment?
-
+// I need discuss in this class when make a meet
 
 import 'package:flutter/material.dart';
+class ScreenUtil {
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  // Getter to access MediaQueryData when context is available
+  static MediaQueryData get mediaQueryData {
+    final context = navigatorKey.currentContext;
+    if (context == null) {
+      throw Exception("Navigator context is not available yet.");
+    }
+    return MediaQuery.of(context);
+  }
+
+  static double width(num value) => value * mediaQueryData.size.width / 100;
+  static double height(num value) => value * mediaQueryData.size.height / 100;
+}
 
 extension FlutterSizes on num {
-  static final nKey = GlobalKey<NavigatorState>();
+  //
+  // static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  //
+  // static final MediaQueryData mediaQueryData =
+  //     MediaQuery.of(navigatorKey.currentState!.context);
 
-  static final MediaQueryData mediaQueryData =
-      MediaQuery.of(nKey.currentState!.context);
+  // static final double screenDeviceWidth = mediaQueryData.size.width;
+  // static final double screenDeviceHeight = mediaQueryData.size.height;
 
-  static final double screenDeviceWidth = mediaQueryData.size.width;
-  static final double screenDeviceHeight = mediaQueryData.size.height;
   static const designWidth = 360;
   static const designHeight = 800;
 
@@ -22,7 +39,7 @@ extension FlutterSizes on num {
 
   SizedBox get addVerticalSpace {
     return SizedBox(
-      height: screenDeviceHeight * (toDouble() / screenDeviceHeight),
+      height: ScreenUtil.height(this) * (toDouble() / ScreenUtil.height(this)),
     );
   }
 
@@ -31,15 +48,15 @@ extension FlutterSizes on num {
   }
 
   double get width {
-    return screenDeviceWidth * (toDouble() / designWidth);
+    return ScreenUtil.width(this) * (toDouble() / designWidth);
   }
 
   double get height {
-    return screenDeviceHeight * (toDouble() / designHeight);
+    return ScreenUtil.width(this) * (toDouble() / designHeight);
   }
 
   double get textSp {
-    return screenDeviceWidth * (toDouble() / designWidth);
+    return ScreenUtil.width(this) * (toDouble() / designWidth);
   }
 
   double get borderRadius {
@@ -52,41 +69,41 @@ extension FlutterSizes on num {
 
   EdgeInsets get paddingHorizontal {
     return EdgeInsets.symmetric(
-        horizontal: screenDeviceWidth * (toDouble() / designWidth));
+        horizontal: ScreenUtil.width(this) * (toDouble() / designWidth));
   }
 
   EdgeInsets get paddingVertical {
     return EdgeInsets.symmetric(
-        vertical: screenDeviceHeight * (toDouble() / designHeight));
+        vertical: ScreenUtil.height(this) * (toDouble() / designHeight));
   }
 
   EdgeInsets get paddingTop {
     return EdgeInsets.only(
-        top: screenDeviceHeight * (toDouble() / designHeight));
+        top: ScreenUtil.height(this) * (toDouble() / designHeight));
   }
 
   EdgeInsets get paddingBottom {
     return EdgeInsets.only(
-        bottom: screenDeviceHeight * (toDouble() / designHeight));
+        bottom: ScreenUtil.height(this) * (toDouble() / designHeight));
   }
 
   EdgeInsets get paddingLeft {
     return EdgeInsets.only(
-        left: screenDeviceWidth * (toDouble() / designWidth));
+        left: ScreenUtil.width(this) * (toDouble() / designWidth));
   }
 
   EdgeInsets get paddingRight {
     return EdgeInsets.only(
-        right: screenDeviceWidth * (toDouble() / designWidth));
+        right: ScreenUtil.width(this) * (toDouble() / designWidth));
   }
 
   EdgeInsets get paddingFromLTRB {
     final value = toDouble();
     return EdgeInsets.fromLTRB(
-      screenDeviceWidth * (value / designWidth),
-      screenDeviceHeight * (value / designHeight),
-      screenDeviceWidth * (value / designWidth),
-      screenDeviceHeight * (value / designHeight),
+      ScreenUtil.width(this) * (value / designWidth),
+      ScreenUtil.height(this) * (value / designHeight),
+      ScreenUtil.width(this) * (value / designWidth),
+      ScreenUtil.height(this) * (value / designHeight),
     );
   }
 
@@ -96,41 +113,41 @@ extension FlutterSizes on num {
 
   EdgeInsets get marginHorizontal {
     return EdgeInsets.symmetric(
-        horizontal: screenDeviceWidth * (toDouble() / designWidth));
+        horizontal: ScreenUtil.width(this) * (toDouble() / designWidth));
   }
 
   EdgeInsets get marginVertical {
     return EdgeInsets.symmetric(
-        vertical: screenDeviceHeight * (toDouble() / designHeight));
+        vertical: ScreenUtil.height(this) * (toDouble() / designHeight));
   }
 
   EdgeInsets get marginTop {
     return EdgeInsets.only(
-        top: screenDeviceHeight * (toDouble() / designHeight));
+        top: ScreenUtil.height(this) * (toDouble() / designHeight));
   }
 
   EdgeInsets get marginBottom {
     return EdgeInsets.only(
-        bottom: screenDeviceHeight * (toDouble() / designHeight));
+        bottom: ScreenUtil.height(this) * (toDouble() / designHeight));
   }
 
   EdgeInsets get marginLeft {
     return EdgeInsets.only(
-        left: screenDeviceWidth * (toDouble() / designWidth));
+        left: ScreenUtil.width(this) * (toDouble() / designWidth));
   }
 
   EdgeInsets get marginRight {
     return EdgeInsets.only(
-        right: screenDeviceWidth * (toDouble() / designWidth));
+        right: ScreenUtil.width(this) * (toDouble() / designWidth));
   }
 
   EdgeInsets get marginFromLTRB {
     final value = toDouble();
     return EdgeInsets.fromLTRB(
-      screenDeviceWidth * (value / designWidth),
-      screenDeviceHeight * (value / designHeight),
-      screenDeviceWidth * (value / designWidth),
-      screenDeviceHeight * (value / designHeight),
+      ScreenUtil.width(this) * (value / designWidth),
+      ScreenUtil.height(this) * (value / designHeight),
+      ScreenUtil.width(this) * (value / designWidth),
+      ScreenUtil.height(this) * (value / designHeight),
     );
   }
 
