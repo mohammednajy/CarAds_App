221db2b8640acece03cf26256743b9881a82a389
ahed <ahedeid2000@gmail.com>
2024-10-27 14:55:16 +0200

    Final commit for FontSize class

--- lib/core/utils/resources/font_size.dart
+++ lib/core/utils/resources/font_size.dart
@@ -4,19 +4,7 @@ class FontConstants {
   static const String fontFamily = "Avenir Arabic";
 }
 
-class FontWeightManager {
-  static const FontWeight black = FontWeight.w900;
-  static const FontWeight extraBold = FontWeight.w800;
-  static const FontWeight bold = FontWeight.w700;
-  static const FontWeight semiBold = FontWeight.w600;
-  static const FontWeight medium = FontWeight.w500;
-  static const FontWeight regular = FontWeight.w400;
-  static const FontWeight light = FontWeight.w300;
-  static const FontWeight extraLight = FontWeight.w200;
-}
-
 class FontSize {
-  static const double s8 = 8.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
   static const double s15 = 15.0;
