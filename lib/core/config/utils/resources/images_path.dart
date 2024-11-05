e78da1b41aac8e1fa3adfe7ea5bc9f95463b2bff
ahed <ahedeid2000@gmail.com>
2024-11-05 14:21:26 +0200

    Add images to auth feature

--- lib/core/config/utils/resources/images_path.dart
+++ lib/core/config/utils/resources/images_path.dart
@@ -1,9 +1,17 @@
 class ImagePath {
   ImagePath._();
+
   static const String imagePath = 'assets/images/';
 
   static const String onboarding1 = '${imagePath}onboarding1.png';
   static const String onboarding2 = '${imagePath}onboarding2.png';
   static const String onboarding3 = '${imagePath}onboarding3.png';
   static const String carSplash = '${imagePath}car_splash.png';
+  static const String apple = '${imagePath}apple.svg';
+  static const String carYallow = '${imagePath}car_yallow.svg';
+  static const String facebook = '${imagePath}facebook.svg';
+  static const String forgetImage = '${imagePath}forget_image.svg';
+  static const String google = '${imagePath}google.svg';
+  static const String newPasswordImage = '${imagePath}new_password_image.svg';
+  static const String otpImage = '${imagePath}otp_image.svg';
 }
